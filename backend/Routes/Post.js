5130b659288e6ba1e55f9e8e87b235c4fa89e43c
Soso-C <sonny.capisciolti@gmail.com>
2021-11-28 20:53:22 +0100

    add modify/delete/getbyid POST

--- backend/Routes/Post.js
+++ backend/Routes/Post.js
@@ -7,7 +7,9 @@ const postController = require('../controllers/post_controller');
 // Post
 router.post("/", postController.createPost);
 router.get("/", postController.getAllPosts);
-
+router.get("/:id", postController.getOnePost);
+router.put("/:id", postController.modifyPost);
+router.delete("/:id", postController.deletePost);
 
 // Comments
 
--- backend/controllers/post_controller.js
+++ backend/controllers/post_controller.js
@@ -1,8 +1,8 @@
 const db = require("../config/db");
 
+
 // Créer un post
 module.exports.createPost = async (req, res) => {
-
   const title = req.body.title;
   const img = req.body.img;
 
@@ -11,10 +11,10 @@ module.exports.createPost = async (req, res) => {
     [title, img],
     (err, results) => {
       if (!err) {
-            res.status(201).json({ message: "Post créé avec succes !" });
-        } else {
-            res.status(500).json({ err });
-        }
+        res.status(201).json({ message: "Post créé avec succes !" });
+      } else {
+        res.status(500).json({ err });
+      }
     }
   );
 };
@@ -22,15 +22,61 @@ module.exports.createPost = async (req, res) => {
 
 // Get tous les posts.
 module.exports.getAllPosts = async (req, res) => {
-    db.query(
-        "SELECT * FROM posts;",
-        (err, result) => {
-          if (err) {
-            res.status(500).json({ err });
-          } else {
-            res.status(200).json(result);
-          }
-        }
-    );
+  db.query("SELECT * FROM posts;", (err, result) => {
+    if (err) {
+      res.status(500).json({ err });
+    } else {
+      res.status(200).json(result);
+    }
+  });
+};
 
-}
+
+// Get 1 post avec son id en params
+module.exports.getOnePost = async (req, res) => {
+  const id = req.params.id;
+  db.query("SELECT * FROM posts WHERE id= ?;", [id], (err, result) => {
+    if (err) {
+      res.status(500).json({ err });
+    } else {
+      res.status(200).json(result);
+    }
+  });
+};
+
+
+// Modify un post
+module.exports.modifyPost = async (req, res) => {
+  const id = req.params.id;
+  const title = req.body.title;
+  const img = req.body.img;
+
+  db.query(
+    "UPDATE posts SET title = ?, img_url = ?  WHERE id = ?;",
+    [title, img, id],
+    (err, result) => {
+      if (err) {
+        res.status(500).json({ err });
+      } else {
+        res.status(200).json({ message: "Modification effectuée ! !" });
+      }
+    }
+  );
+};
+
+
+// Delete un post
+module.exports.deletePost = async (req, res) => {
+    const id = req.params.id;
+  
+    db.query("DELETE FROM posts WHERE id = ?;", 
+        [id], 
+        (err, result) => {
+        if (err) {
+            res.status(500).json({ err });
+        } else {
+            res.status(200).json({ message: "Le post a bien été supprimé !" });
+        }
+    });
+  };
+  
